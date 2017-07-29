2bb73f68cb7c982d3615df127a7d7da9c5684056
Ridwan <ridwan.al.mansur@hotmail.com>
2017-07-29 21:20:12 +0100

    Added XML element names

--- UserSort/Classes/User.cs
+++ UserSort/Classes/User.cs
@@ -3,19 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace UserSort.Classes
 {
     /// <summary>
     /// User class. Object will hold data imported from files 
     /// </summary>
+    [XmlRoot(ElementName = "user")] 
     public class User
     {
+        [XmlElement("userid")]
         public int user_id { get; set; }
+        [XmlElement("firstname")]
         public string first_name { get; set; }
+        [XmlElement("lastname")]
         public string last_name { get; set; }
+        [XmlElement("username")]
         public string username { get; set; }
+        [XmlElement("type")]
         public string user_type { get; set; }
+        [XmlElement("lastlogintime")]
         public string last_login_time { get; set; }
 
         public User() { }
